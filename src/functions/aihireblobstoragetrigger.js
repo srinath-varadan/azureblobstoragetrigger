8516e907ead6f698c9bdaf2e4aaf2a7d798169de
srinathvaradan <srinathvaradan@gmail.com>
2024-03-09 20:16:13 +0530

    BlobStorage

---
+++ src/functions/aihireblobstoragetrigger.js
@@ -0,0 +1,92 @@
+const { app, input, output } = require("@azure/functions");
+const { v4: uuidv4 } = require("uuid");
+const { ApiKeyCredentials } = require("@azure/ms-rest-js");
+const sleep = require("util").promisify(setTimeout);
+const {
+  DocumentAnalysisClient,
+  AzureKeyCredential,
+} = require("@azure/ai-form-recognizer");
+const {
+  TextAnalysisClient,
+  AzureKeyCredential: nerAzureKeyCredential,
+} = require("@azure/ai-language-text");
+
+const fs = require("fs");
+
+async function analyzeDocument(blob) {
+  const endpoint = "https://aihiredi.cognitiveservices.azure.com/";
+  const apiKey = "62d2626f91aa4356b2182266e268f11c";
+  const modelId = "prebuilt-read";
+  const client = new DocumentAnalysisClient(
+    endpoint,
+    new AzureKeyCredential(apiKey)
+  );
+  const poller = await client.beginAnalyzeDocument(modelId, blob);
+  const { content, pages, languages } = await poller.pollUntilDone();
+  let pageContent = "";
+  if (!pages || pages.length <= 0) {
+    console.log("No pages were extracted from the document.");
+  } else {
+    console.log("Pages:");
+    for (const page of pages) {
+      if (page.lines && page.lines.length > 0) {
+        console.log("  Lines:");
+        for (const line of page.lines) {
+          pageContent += line.content;
+          pageContent += " ";
+        }
+      }
+    }
+  }
+
+  console.log(pageContent);
+
+  const nerKey = "81bc8ec5058947038df270d162b30a2b";
+  const nerEndPoint = "https://aihirelangresource.cognitiveservices.azure.com/";
+  const nerClient = new TextAnalysisClient(
+    nerEndPoint,
+    new nerAzureKeyCredential(nerKey)
+  );
+
+  const results = await nerClient.analyze("EntityRecognition", [pageContent]);
+
+  return results;
+}
+
+app.storageBlob("aihireblobstoragetrigger", {
+  path: "aihirecontainer/{name}",
+  connection: "aihireblob_STORAGE",
+  handler: async (blob, context) => {
+    context.log(
+      `Storage blob function processed blob "${context.triggerMetadata.name}" with size ${blob.length} bytes`
+    );
+    const blobUrl = context.triggerMetadata.uri;
+    const extension = blobUrl.split(".").pop();
+    if (!blobUrl) {
+      // url is empty
+      return;
+    } else {
+      //url is image
+      const id = uuidv4().toString();
+
+      const analysis = await analyzeDocument(blob);
+
+      // `type` is the partition key
+      const dataToInsertToDatabase = {
+        id,
+        type: extension,
+        blobUrl,
+        blobSize: blob.length,
+        data: analysis,
+        trigger: context.triggerMetadata,
+      };
+      console.log(dataToInsertToDatabase);
+      return dataToInsertToDatabase;
+    }
+  },
+  return: output.cosmosDB({
+    connection: "CosmosDBConnection",
+    databaseName: "aihirecosmosDB",
+    containerName: "Documents",
+  }),
+});
